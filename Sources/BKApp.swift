73c5bc90453276f2b0b4b4542477d769546efd4f
ApolloZhu <public-apollonian@outlook.com>
2019-02-10 00:45:07 -0500

    [Result] BKArticle.Info

--- Sources/BKApp.swift
+++ Sources/BKApp.swift
@@ -7,20 +7,6 @@
 
 import Foundation
 
-public enum BKError: Error {
-    public enum ParseErrorReason {
-        case regexMatchNotFound
-    }
-    public enum ResponseErrorReason {
-        case urlSessionError(Error?)
-        case stringDecodeFailure
-    }
-    
-    case implementationError(Error)
-    case parseError(reason: ParseErrorReason)
-    case responseError(reason: ResponseErrorReason)
-}
-
 /// APPKEY associated operations
 public enum BKApp {
     /// APPKEY from bilibili website.
@@ -35,13 +21,14 @@ public enum BKApp {
     ///
     /// - Parameter handler: code to run with fetched appkey.
     public static func fetchKey(_ handle: @escaping (Result<String, BKError>) -> Void) {
-        let task = URLSession.bk.dataTask(with: playerURL) { data, _, err in
+        let task = URLSession.bk.dataTask(with: playerURL) { data, res, err in
             handle(regex.mapError { .implementationError($0) }.flatMap { regex in
                 guard let data = data else {
-                    return .failure(.responseError(reason: .urlSessionError(err)))
+                    return .failure(.responseError(
+                        reason: .urlSessionError(err, response: res)))
                 }
                 guard let raw = String(data: data, encoding: .utf8) else {
-                    return .failure(.responseError(reason: .stringDecodeFailure))
+                    return .failure(.parseError(reason: .stringDecodeFailure))
                 }
                 let range = NSRange(raw.indices.startIndex..<raw.indices.endIndex, in: raw)
                 guard let match = regex.matches(in: raw, range: range).first else {
